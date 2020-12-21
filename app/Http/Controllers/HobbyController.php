e86a6c716c05da3b70feede8fc1fc61860a03b69
leeling12 <75717007+leeling12@users.noreply.github.com>
2020-12-21 21:35:44 +0800

    test @auth, @guest, redirect page

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -72,11 +72,16 @@ class HobbyController extends Controller
         ]);
         $hobby->save();
         //redirect back index page
-        return $this->index()->with(
+        /*return $this->index()->with(
             [
                 'message_success' => "The hobby <b>". $hobby->name . "</b> was created."
             ]
-        );
+        );*/
+        return redirect('/hobby/'.$hobby->id)->with( //redirect to hobby details page
+            [
+                'message_warning' => "Please assign some tags now."
+            ]
+        ); 
     }
 
     /**
@@ -94,7 +99,8 @@ class HobbyController extends Controller
         return view('hobby.show')->with([
             'hobby' => $hobby,
             'availableTags' => $availableTags,
-            'message_success' => Session::get('message_success') //flash session
+            'message_success' => Session::get('message_success'), //flash session
+            'message_warning' => Session::get('message_warning')
         ]);
     }
 
