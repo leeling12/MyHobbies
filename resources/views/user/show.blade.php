7ec11bc77db36718b43a17091fc6d758b05b3372
leeling12 <75717007+leeling12@users.noreply.github.com>
2020-12-22 00:33:54 +0800

    show hobbies in user page

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -5,14 +5,38 @@
     <div class="row justify-content-center">
         <div class="col-md-11">
             <div class="card">
-                <div class="card-header">{{ $user->name }}</div>
+                <div class="card-header" style="font-size: 150%">{{ $user->name }}</div>
 
                 <div class="card-body">
                    <p><b>My Motto:</b><br>{{ $user->motto }}</p>
                    <p class="mt-2"><b>Abour Me:</b><br>{{ $user->about_me }}</p>
+
+                   @if($user->hobbies->count() > 0)
+                    <h5 class="mt-4">Hobbies of {{ $user->name }} </h5>
+                    <ul class="list-group">
+                            @foreach($user->hobbies as $hobby)
+                                    <li class="list-group-item">
+                                        <a style="color:black;" title="Show Details" href="/hobby/{{$hobby->id}}"><b>{{ $hobby->name }}</b></a>
+                                    
+                                        <!-- display date -->
+                                        <span class="float-right mx-2"> {{ $hobby->updated_at->diffForHumans()}}</span>
+
+                                        <!-- display tags -->
+                                        <br>
+                                        @foreach($hobby->tags as $tag)
+                                            <a href="/hobby/tag/{{$tag->id}}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
+                                        @endforeach
+                                    </li>
+                                @endforeach
+                    </ul>
+                @else
+                    <p>{{ $user->name }} has not created any hobbies yet.</p>
+                @endif
+
                 </div>
             </div>
 
+  
             <div class="mt-2">
                 <a class="btn btn-primary btn-sm" href="{{ URL::previous() }}"><i class="fas fa-arrow-circle-left"></i> Back to Overview</a>
             </div>
